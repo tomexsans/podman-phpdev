10c68f1648537a4617029165a33f5ec7c67166af
Tomexsans <t2t.exe@gmail.com>
2025-01-02 12:46:49 +0800

    UPDATED INIT.sh

--- init.sh
+++ init.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
 # Set pod name
-POD_NAME=phpdev3-env
+POD_NAME=PHPDEVTOM
 CONTAINER_ALIAS=${POD_NAME}-cont-
 
+# Define ALl Ports to be used container:host
+PORTS=(
+  "80:80" #Nginx
+  "8080:8080" #used default by laravel app
+  "5432:5432" #postgresql
+  "11184:11184" # will be used as port for php-fpm latest
+  "11185:11185" # will be used as port for php-fpm8.3
+  "11211:11211" # memcached
+  "5173:5173" #vite
+  "5174:5174" #vite
+)
+
 # Create necessary directories
-mkdir -p nginx/config
-mkdir -p php/config
-mkdir -p postgres/data
+mkdir -p config/nginx/config
+mkdir -p config/php/config
+mkdir -p config/postgres/data
 mkdir -p src/html
 
 # Create NGINX default.conf
-cat <<EOF > ./nginx/config/default.conf
+cat <<EOF > $(pwd)config/nginx/config/default.conf
 server {
     listen 80;  # Listen on port 80 for HTTP traffic
     server_name localhost;  # Server name or IP address
@@ -31,6 +43,14 @@ server {
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
     }
 
+    ## Sample template to support other PHP versions multiple
+    #location ~ \.php$ {
+    #    include fastcgi_params;
+    #    fastcgi_pass ${CONTAINER_ALIAS}php-fpm8.3:9000;  # Name of the PHP-FPM container and port (inside the pod)
+    #    fastcgi_index index.php;
+    #    fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+    #}
+
     error_page 404 /404.html;
     location = /404.html {
         root /usr/share/nginx/html;
@@ -39,14 +59,14 @@ server {
 EOF
 
 # Create custom PHP configuration files
-cat <<EOF > $(pwd)/php/config/custom.ini
+cat <<EOF > $(pwd)config/php/config/custom.ini
 # Disable opcache in the container, if not PHP files are cached
 opcache.enable=0
 opcache.enable_cli=0
 # Enable other Extensions
 extension=pdo_pgsql
 EOF
-touch $(pwd)/php/config/php-fpm.conf
+touch $(pwd)config/php/config/php-fpm.conf
 
 # Create a simple HTML page in src/html/index.html
 cat <<EOF > $(pwd)/src/html/index.html
@@ -72,26 +92,43 @@ echo "Greetings from PODMAN Container (PHP is Running!)";
 EOF
 
 # Create the Pod with the PORTS
-podman pod create --name ${POD_NAME} -p 80:80 -p 8080:8080 -p 5432:5432 \
-    -p 9000:9000 -p 11211:11211 \
-    -p 5173:5173 -p 5174:5174
+# Port 74784 will be used for PHP-FPM socket to parse php-8.4
+# Add ports here if you plan to support multiple PHP 74783 is added also
+# Create the pod with the ports
+POD_CREATE_CMD="podman pod create --name ${POD_NAME}"
+
+for PORT in "${PORTS[@]}"; do
+  POD_CREATE_CMD+=" -p $PORT"
+done
+# Execute the command
+echo "Executing: $POD_CREATE_CMD"
+eval "$POD_CREATE_CMD"
 
 # Adjust for Rootless Podman and ports < 1024
 sudo sysctl net.ipv4.ip_unprivileged_port_start=80
 
 # Create PostgreSQL Container
 podman run -d --pod ${POD_NAME} --name ${CONTAINER_ALIAS}postgres \
-    -v $(pwd)/postgres/data:/var/lib/postgresql/data \
+    -v $(pwd)/postgres/data:/bitnami/postgresql \
     -e POSTGRES_USER=user \
     -e POSTGRES_PASSWORD=pass \
     -e POSTGRES_DB=mydatabase \
     bitnami/postgresql
 
-# Create PHP-FPM Container
+# Create PHP-FPM Container for PHP Latest version, bind to port 74784
 podman run -d --pod ${POD_NAME} --name ${CONTAINER_ALIAS}php-fpm \
     --mount type=bind,source=$(pwd)/src,target=/var/www,bind-propagation=rshared \
     --mount type=bind,source=$(pwd)/php/config,target=/opt/bitnami/php/etc/conf.d,bind-propagation=rshared \
     bitnami/php-fpm
+
+## Create PHP-FPM container for a specific php version
+## https://hub.docker.com/r/bitnami/php-fpm
+podman run -d --pod ${POD_NAME} --name ${CONTAINER_ALIAS}php-fpm8.3 \
+    --mount type=bind,source=$(pwd)/src,target=/var/www,bind-propagation=rshared \
+    --mount type=bind,source=$(pwd)/php/config,target=/opt/bitnami/php/etc/conf.d,bind-propagation=rshared \
+    bitnami/php-fpm:8.3
+
+
 
 # Create Nginx Container
 podman run -d --pod ${POD_NAME} --name ${CONTAINER_ALIAS}nginx \
